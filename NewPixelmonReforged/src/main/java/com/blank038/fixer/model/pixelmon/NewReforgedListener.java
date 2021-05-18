1b47991bc524eb618a07e714b0f6c3d1fdb7a3b0
Blank038 <admin@mc9y.com>
2021-05-18 15:45:30 +0800

    修复 8.2.0 剑盾形态切换属性及种族值不变

--- NewPixelmonReforged/src/main/java/com/blank038/fixer/model/pixelmon/NewReforgedListener.java
+++ NewPixelmonReforged/src/main/java/com/blank038/fixer/model/pixelmon/NewReforgedListener.java
@@ -1,48 +1,46 @@
 package com.blank038.fixer.model.pixelmon;
 
 import com.blank038.fixer.Fixer;
+import com.blank038.fixer.model.pixelmon.stats.FixStatsEnum;
 import com.mc9y.pokemonapi.api.event.ForgeEvent;
-import com.pixelmonmod.pixelmon.Pixelmon;
 import com.pixelmonmod.pixelmon.api.events.battles.AttackEvent;
-import com.pixelmonmod.pixelmon.api.events.moveskills.UseMoveSkillEvent;
-import com.pixelmonmod.pixelmon.api.pokemon.Pokemon;
-import com.pixelmonmod.pixelmon.entities.pixelmon.EntityPixelmon;
+import com.pixelmonmod.pixelmon.entities.pixelmon.stats.BaseStats;
+import com.pixelmonmod.pixelmon.entities.pixelmon.stats.BaseStatsLoader;
 import com.pixelmonmod.pixelmon.entities.pixelmon.stats.StatsType;
-import com.pixelmonmod.pixelmon.enums.EnumSpecies;
-import com.pixelmonmod.pixelmon.storage.PlayerPartyStorage;
-import org.bukkit.Bukkit;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import java.util.LinkedHashMap;
 
 /**
  * @author Blank038
  * @since 2021-05-14
  */
 public class NewReforgedListener extends ReforgedListener implements Listener {
-    private final List<String> UUIDS = new ArrayList<>();
-    private final HashMap<StatsType, Integer> ZACIAN_STATS = new HashMap<>();
 
     public NewReforgedListener() {
-        this.ZACIAN_STATS.put(StatsType.HP, 92);
-        this.ZACIAN_STATS.put(StatsType.Attack, 170);
-        this.ZACIAN_STATS.put(StatsType.Defence, 115);
-        this.ZACIAN_STATS.put(StatsType.SpecialAttack, 80);
-        this.ZACIAN_STATS.put(StatsType.SpecialDefence, 115);
-        this.ZACIAN_STATS.put(StatsType.Speed, 148);
-        // 启用线程
-        Bukkit.getScheduler().runTaskTimerAsynchronously(Fixer.getInstance(), () -> {
-            for (Player player : Bukkit.getOnlinePlayers()) {
-                PlayerPartyStorage partyStorage = Pixelmon.storageManager.getParty(player.getUniqueId());
-                for (Pokemon pokemon : partyStorage.getTeam()) {
-
+        if (Fixer.getConfiguration().getBoolean("message.pixelmon.form_stats.enable")) {
+            // 增加数据
+            try {
+                StatsType[] statsTypes = StatsType.getStatValues();
+                for (FixStatsEnum fse : FixStatsEnum.values()) {
+                    BaseStats formStats = BaseStatsLoader.getBaseStatsFromAssets(fse.getSpecies());
+                    // 设置种族值
+                    for (int i = 0; i < 6; i++) {
+                        formStats.stats.replace(statsTypes[i], fse.getStats(i));
+                    }
+                    // 设置属性
+                    formStats.types = new ArrayList<>(fse.getTypes());
+                    // 将基础数据类增加至形态集合
+                    fse.getSpecies().getBaseStats().forms = new LinkedHashMap<>();
+                    fse.getSpecies().getBaseStats().forms.put(1, formStats);
                 }
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-        }, 20L, 20L);
+        }
     }
 
     @Override
@@ -51,32 +49,14 @@ public class NewReforgedListener extends ReforgedListener implements Listener {
         if (event.getForgeEvent() instanceof AttackEvent.Use && skillCopyItem) {
             AttackEvent.Use e = (AttackEvent.Use) event.getForgeEvent();
             if (e.user.getPlayerOwner() != null && e.target.getPlayerOwner() == null && !e.target.isMega) {
-                if ("Covet".equals(e.getAttack().getMove().getAttackName()) && e.target.hasHeldItem() && BATTLE_CONTROLLER_MAP.remove(e.target.getPokemonUUID().toString()) != null) {
+                if ("Covet".equals(e.getAttack().getMove().getAttackName()) && e.target.hasHeldItem()
+                        && BATTLE_CONTROLLER_MAP.remove(e.target.getPokemonUUID().toString()) != null) {
                     e.target.setNewHeldItem(null);
                     Fixer.getInstance().getLogger().info("尝试防止玩家技能复制物品, 触发玩家: " + e.user.getPlayerOwner().getName());
                 } else if ("Bestow".equals(e.getAttack().getMove().getAttackName())) {
                     BATTLE_CONTROLLER_MAP.put(e.target.getPokemonUUID().toString(), e.target.bc);
                 }
             }
-        } else if (event.getForgeEvent() instanceof UseMoveSkillEvent) {
-            UseMoveSkillEvent e = (UseMoveSkillEvent) event.getForgeEvent();
-            if ("crowned".equals(e.moveSkill.id) && e.pixelmon.getSpecies() == EnumSpecies.Zacian) {
-                this.setZacianStats(e.pixelmon);
-                Bukkit.getScheduler().runTask(Fixer.getInstance(), () -> {
-                    this.setZacianStats(e.pixelmon);
-                });
-            }
-        }
-    }
-
-    private void setZacianStats(EntityPixelmon pixelmon) {
-        if (pixelmon != null && pixelmon.getSpecies() == EnumSpecies.Zacian) {
-            System.out.println(pixelmon.getFormIncludeTransformed());
-            System.out.println("----");
-            pixelmon.getSpecies().getDefaultForms().forEach((k) -> System.out.println(k.getForm()));
-            System.out.println("----");
-            pixelmon.getSpecies().getBaseStats(pixelmon.getFormEnum()).evYields.forEach((k, v) -> System.out.println(k.name() + " - " + v));
-            System.out.println(pixelmon.getFormEnum().isDefaultForm());
         }
     }
 }
---
+++ NewPixelmonReforged/src/main/java/com/blank038/fixer/model/pixelmon/stats/FixStatsEnum.java
@@ -0,0 +1,38 @@
+package com.blank038.fixer.model.pixelmon.stats;
+
+import com.pixelmonmod.pixelmon.enums.EnumSpecies;
+import com.pixelmonmod.pixelmon.enums.EnumType;
+import scala.actors.threadpool.Arrays;
+
+import java.util.List;
+
+/**
+ * @author Blank038
+ * @since 2021-05-18
+ */
+public enum FixStatsEnum {
+    Zacian(EnumSpecies.Zacian, new int[]{92, 170, 115, 80, 115, 148}, Arrays.asList(new EnumType[]{EnumType.Fairy, EnumType.Steel})),
+    Zamazenta(EnumSpecies.Zamazenta, new int[]{92, 130, 145, 80, 145, 128}, Arrays.asList(new EnumType[]{EnumType.Fighting, EnumType.Steel}));
+
+    private final EnumSpecies species;
+    private final int[] array;
+    private final List<EnumType> types;
+
+    FixStatsEnum(EnumSpecies species, int[] array, List<EnumType> types) {
+        this.species = species;
+        this.array = array;
+        this.types = types;
+    }
+
+    public EnumSpecies getSpecies() {
+        return this.species;
+    }
+
+    public int getStats(int index) {
+        return this.array[index];
+    }
+
+    public List<EnumType> getTypes() {
+        return this.types;
+    }
+}
